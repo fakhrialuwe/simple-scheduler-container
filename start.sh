3d86fc7ad9cb6b25f1507ca0e8803636a97caec8
Mohd Fakhri Aluwe <42363806+fakhrialuwe@users.noreply.github.com>
2023-06-13 15:06:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ if [ ! -f /tmp/cmsrevalidate-$(date +%Y%m%d) ]; then
     php -f send_email.php
 # Draft claim email disabled by commenting below:
 #    npm run draft
-    touch /tmp/cmsrevalidate-$(date +%Y%m%d)
+    touch /tmp/sendemail-revalidate-$(date +%Y%m%d)
 else
     echo "We have run the script today. Skip."
 fi
